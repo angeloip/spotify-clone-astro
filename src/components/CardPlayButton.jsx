9a3dc4f3b92063a1463d952e476806a33f1e8538
AngeloIngaJP <angelo.inga@unmsm.edu.pe>
2023-12-21 22:38:26 -0500

    spotify-clone-astro

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,7 +1,7 @@
 import { usePlayerStore } from '@/store/playerStore'
 import { Pause, Play } from './Player'
 
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size = 'small' }) {
   const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state)
 
@@ -22,12 +22,18 @@ export function CardPlayButton({ id }) {
       })
   }
 
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
+
   return (
     <button
       onClick={handleClick}
       className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400"
     >
-      {isPlayingPlaylist ? <Pause /> : <Play />}
+      {isPlayingPlaylist ? (
+        <Pause className={iconClassName} />
+      ) : (
+        <Play className={iconClassName} />
+      )}
     </button>
   )
 }
